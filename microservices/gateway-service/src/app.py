0e1f567e5503f2042c271005f2a85c339d40f6a2
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2024-08-06 15:09:59 +0100

    Update app.py

--- microservices/gateway-service/src/app.py
+++ microservices/gateway-service/src/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return " Hello from adria : using image update automation : v 3.5.0-34"
+    return " Hello from adria : using image update automation : v 3.5.0-77"
